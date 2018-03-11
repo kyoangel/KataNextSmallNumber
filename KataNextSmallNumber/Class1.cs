121184ca80e280a382e936522012f73b9c248349
kyo.lai82 <kyo.lai82@titansoft.com.sg>
2018-03-11 16:51:15 +0800

    Add project files.

---
+++ KataNextSmallNumber/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KataNextSmallNumber
+{
+    public class Class1
+    {
+    }
+}
