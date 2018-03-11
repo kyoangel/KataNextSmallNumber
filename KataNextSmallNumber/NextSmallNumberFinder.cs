366b56c5ac5c4bd4784ee06a45f59604cb96784f
kyo.lai82 <kyo.lai82@titansoft.com.sg>
2018-03-11 16:53:17 +0800

    complete class NextSamllNyumberFider and its method NextSmallNumber(long)

---
+++ KataNextSmallNumber/NextSmallNumberFinder.cs
@@ -0,0 +1,10 @@
+﻿namespace KataNextSmallNumber
+{
+    public class NextSmallNumberFinder
+    {
+        public static long NextSmallNumber(long targetNumber)
+        {
+            return 0;
+        }
+    }
+}
